eb52e23b5cd15894f6dd9f688cd69489010ab30f
Sompetch Katisomsakul <sompetch0249@gmail.com>
2020-01-19 11:12:25 +0700

    saperate element of tic-tac-toe

--- src/components/tic-tac-toe/tic-tac-toe.js
+++ src/components/tic-tac-toe/tic-tac-toe.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Board from "./Board";
 
 function calculateWinner(squares) {
   const lines = [
@@ -20,45 +21,6 @@ function calculateWinner(squares) {
   return null;
 }
 
-function Square(props) {
-  return (
-    <button className="square" onClick={() => props.onClick()}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 export default class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -96,7 +58,7 @@ export default class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[this.state.stepNumber];
+    const current = history[this.state];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
